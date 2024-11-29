041bf1238faea018b52fa4ff504b2e2c5a680d2d
Lea-Far-7 <lea.fargeot33@gmail.com>
2024-11-29 15:45:14 +0100

    Ajout de la méthode pour lister des répertoires dans FileManager + modification du main

--- Modules/FileManager.py
+++ Modules/FileManager.py
@@ -13,12 +13,24 @@ class FileManager :
 
     def lister_fichiers(self, repertoire : str) -> list:
         try:
+            # Récupère tous les éléments dans le répertoire
             contenu_repertoire = listdir(repertoire)
             # Filtrage pour ne garder que les fichiers
             fichiers = [f for f in contenu_repertoire if path.isfile(path.join(repertoire, f))]
             return fichiers
         except FileNotFoundError:
             print(f"Le répertoire {repertoire} n'existe pas.")
+            return []
+
+    def lister_dossiers(self, repertoire : str) -> list:
+        try:
+            # Récupère tous les éléments dans le répertoire
+            contenu_repertoire = listdir(repertoire)
+            # Filtrage pour ne garder que les dossiers
+            dossiers = [d for d in contenu_repertoire if path.isdir(path.join(repertoire, d))]
+            return dossiers
+        except FileNotFoundError:
+            print(f"Le répertoire {contenu_repertoire} n'existe pas.")
             return []
 
 """
