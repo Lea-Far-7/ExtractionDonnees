887ee7545fe604ac43082d2b3c9ed7404222e273
De almeida <dealmeidamateo@gmail.com>
2024-12-06 14:25:17 +0100

    Réarrangement du code et l'import est maintenant correct pour afficher les marqueurs

--- Interface/InterfacePrincipale.py
+++ Interface/InterfacePrincipale.py
@@ -12,14 +12,14 @@ from Modules.DataExtractor import DataExtractor
 # Modes: "System", "Dark", "Light"
 customtkinter.set_appearance_mode("Dark")
 
-# Thèmes: "blue", "green", "dark-blue"
-customtkinter.set_default_color_theme("blue")
-
-
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+
+        # Définition des composants essentiels
+        self.person_var_name = {}
+        self.tableau = None
 
         # Permet de mettre un titre et de définir la taille originale de la fenêtre
         self.title("OLOCAP Viewer")
@@ -31,9 +31,18 @@ class App(customtkinter.CTk):
         self.image_client = ImageTk.PhotoImage(Image.open(os.path.join(self.current_path, "../Images", "client.png")).resize((35, 35)))
         self.image_producteur = ImageTk.PhotoImage(Image.open(os.path.join(self.current_path, "../Images", "producteur.png")).resize((35, 35)))
 
+        # Crée une Image mise dans un Label pour être affichée et contenue dans la sidebar
+        self.my_image = customtkinter.CTkImage(light_image=Image.open("../Images/Logo_Olocap.png"), dark_image=Image.open("../Images/Logo_Olocap.png"), size=(192,58))
+
         # Mise en arrière définitive de la fenêtre (pour que les pop-ups puissent toujours se situer devant)
         self.attributes('-topmost',False)
 
+        self.interfacePrincipale()
+
+        self.creationCarte()
+        self.creationTableau()
+
+    def interfacePrincipale(self):
         # Permet de configurer la grid,
         # "weight = 1" permet de remplir au maximum l'espace de la grid désigné
         # "weight = 0" permet de réduire au minimum l'espace de la grid désigné
@@ -45,37 +54,41 @@ class App(customtkinter.CTk):
         self.sidebar = customtkinter.CTkFrame(self, width=230, corner_radius=0)
         self.sidebar.grid(row=0, column=0, rowspan=4, sticky="nsew")
 
-        # Crée une Image mise dans un Label pour être affichée et contenue dans la sidebar
-        self.my_image = customtkinter.CTkImage(light_image=Image.open("../Images/Logo_Olocap.png"), dark_image=Image.open("../Images/Logo_Olocap.png"), size=(192,58))
-
+        # Création de l'image logo
         self.image_label = customtkinter.CTkLabel(self.sidebar, image=self.my_image, text=" ")
         self.image_label.grid(row=0, column=0, padx=20, pady=(20, 10))
 
-        self.couleur_bouton = customtkinter.CTkImage(light_image=Image.open('../Images/Couleur_Boutons.png'), size=(500, 150))
+        # Utilisation d'une image contenant une seule couleur pour l'appliquer sur les boutons
+        self.couleur_bouton = customtkinter.CTkImage(light_image=Image.open('../Images/Couleur_Boutons.png'),
+                                                     size=(500, 150))
 
-        # Création des boutons dans la sidebar ( soit popupImportCreate, soit importVer2)
-        self.importer = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855", command = lambda : PopupImport(self) , text = "Importer")
+        # Création des boutons dans la sidebar
+        self.importer = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855",
+                                                command=lambda: PopupImport(self), text="Importer")
         self.importer.grid(row=1, column=0, padx=20, pady=10)
-
-        self.filtre = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855",  command=self.sidebar_button_event, text = "Filtres")
+        self.filtre = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855",
+                                              command=self.sidebar_button_event, text="Filtres")
         self.filtre.grid(row=4, column=0, padx=20, pady=10)
-
-        self.export = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855", command=self.sidebar_button_event, text = "Export")
+        self.export = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855",
+                                              command=self.sidebar_button_event, text="Export")
         self.export.grid(row=6, column=0, padx=20, pady=10)
-
-        self.ajout = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855", command=self.sidebar_button_event, text = "Ajout")
+        self.ajout = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855",
+                                             command=self.sidebar_button_event, text="Ajout")
         self.ajout.grid(row=7, column=0, padx=20, pady=10)
-
-        self.supprime = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855", command=self.sidebar_button_event, text = "Supprimer")
+        self.supprime = customtkinter.CTkButton(self.sidebar, fg_color="#1d7c69", hover_color="#275855",
+                                                command=self.sidebar_button_event, text="Supprimer")
         self.supprime.grid(row=8, column=0, padx=20, pady=10)
 
         # Création des boutons de navigation entre la map et le tableau
-        self.button_map = customtkinter.CTkButton(self, fg_color="#1d7c69", hover_color="#275855", command=self.select_map, text="Carte")
+        self.button_map = customtkinter.CTkButton(self, fg_color="#1d7c69", hover_color="#275855",
+                                                  command=self.select_map, text="Carte")
         self.button_map.grid(row=0, column=2, sticky="e", padx=5, pady=5)
 
-        self.button_tableau = customtkinter.CTkButton(self, fg_color="#1d7c69", hover_color="#275855", command=self.select_tab, text="Tableau")
+        self.button_tableau = customtkinter.CTkButton(self, fg_color="#1d7c69", hover_color="#275855",
+                                                      command=self.select_tab, text="Tableau")
         self.button_tableau.grid(row=0, column=3, sticky="w", padx=5, pady=5)
 
+    def creationCarte(self):
         # Création de la map et mise dans une grid (ligne 1 et colonne 1)
         self.map_widget = TkinterMapView()
         self.map_widget.grid(row=1, rowspan=8, column=1, columnspan=4, sticky="nsew")
@@ -84,6 +97,7 @@ class App(customtkinter.CTk):
         # Définition de la position de départ
         self.map_widget.set_position(47.3565655, 0.7035767)
 
+    def creationTableau(self):
         # Création du tableau
         self.tableau = ttk.Treeview()
         self.tableau['columns'] = ["Producteur","Client","Poids Maximal","Demi-Jour travaillé ?","Autre"]
@@ -106,24 +120,24 @@ class App(customtkinter.CTk):
 
     def valider(self):
 
-        print(self.fichier_producteurs)
+        for element in self.person_var_name.keys() :
+            self.person_var_name[element].delete()
+
         donnees = CreerClasses(self.fichier_producteurs)
 
-        self.person_var_name = {}
+
         for person in donnees.getProducteurs():
-            if (1==1) :
-                self.person_var_name[person.id] = self.map_widget.set_marker(person.latitude, person.longitude, icon=self.image_producteur,
+            self.person_var_name[person.id] = self.map_widget.set_marker(person.latitude, person.longitude, icon=self.image_producteur,
                                                        command=self.showDataMarker)
 
         for person in donnees.getClients():
-            if (1 == 1):
-                self.person_var_name[person.id] = self.map_widget.set_marker(person.latitude, person.longitude,
+            self.person_var_name[person.id] = self.map_widget.set_marker(person.latitude, person.longitude,
                                                                                  icon=self.image_client,
                                                                                  command=self.showDataMarker)
 
 
-        print(donnees.getProducteurs())
-        print(donnees.getClients())
+        # print(donnees.getProducteurs())
+        # print(donnees.getClients())
         for selected in self.choixSolutions:
             if (selected.get() == 1) :
                 print(selected.cget("text"))
